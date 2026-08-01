6e513bcc9c69020185f10c46772a95e8ae90fd18
Nikunj <1032241456@tcetmumbai.in>
2026-08-01 23:21:56 +0530

    Initial commit

---
+++ src/main/java/com/parking/SmartVehicleParkingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVehicleParkingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartVehicleParkingManagementSystemApplication.class, args);
+    }
+
+}
